67d3d5c1d9836fd52c4f38cd964e017de284c55b
doquangtrung <doquangtrung1072006@gmail.com>
2026-01-30 15:55:42 +0700

    dkydangnhap

--- app/src/main/java/trungfpt/quanlyduanagile/DAO/DBHelper.java
+++ app/src/main/java/trungfpt/quanlyduanagile/DAO/DBHelper.java
@@ -1,4 +1,22 @@
 package trungfpt.quanlyduanagile.DAO;
 
-public class DBHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
