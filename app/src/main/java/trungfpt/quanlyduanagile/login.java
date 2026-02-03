b37342488ee1d73a78ff6595251b0d94736c2bb8
doquangtrung <doquangtrung1072006@gmail.com>
2026-02-03 12:42:48 +0700

    dkydangnhap

--- app/src/main/java/trungfpt/quanlyduanagile/login.java
+++ app/src/main/java/trungfpt/quanlyduanagile/login.java
@@ -15,10 +15,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import trungfpt.quanlyduanagile.DAO.DBHelper;
-
 public class login extends AppCompatActivity {
-    EditText edtUserName;
-    EditText edtPassword;
+
+    EditText edtUserName, edtPassword;
     Button btnSignin;
     TextView tvSignup;
     DBHelper db;
@@ -26,33 +25,37 @@ public class login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
         edtUserName = findViewById(R.id.edtUsername);
         edtPassword = findViewById(R.id.edtPassword);
         btnSignin = findViewById(R.id.btnSignin);
         tvSignup = findViewById(R.id.tvSignup);
 
-        btnSignin.setOnClickListener(v ->{
-            String user = edtUserName.getText().toString();
-            String pass = edtPassword.getText().toString();
+        db = new DBHelper(this);
 
-            if(user.isEmpty() || pass.isEmpty()){
+
+        btnSignin.setOnClickListener(v -> {
+            String user = edtUserName.getText().toString().trim();
+            String pass = edtPassword.getText().toString().trim();
+
+            if (user.isEmpty() || pass.isEmpty()) {
                 Toast.makeText(this, "Không được để trống", Toast.LENGTH_SHORT).show();
                 return;
             }
-            if(db.login(user,pass)){
-                SharedPreferences sp = getSharedPreferences("SESSION",MODE_PRIVATE);
-                sp.edit().putString("USER",user).apply();
-                startActivity(new Intent(this, MainActivity.class));
+
+            if (db.login(user, pass)) {
+                SharedPreferences sp = getSharedPreferences("SESSION", MODE_PRIVATE);
+                sp.edit().putString("USER", user).apply();
+
+                startActivity(new Intent(login.this, dangxuat.class));
                 finish();
             } else {
-            Toast.makeText(this, "Sai tài khoản hoặc mật khẩu", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Sai tài khoản hoặc mật khẩu", Toast.LENGTH_SHORT).show();
             }
-            tvSignup.setOnClickListener(t ->{
-                startActivity(new Intent(this, dangky.class));
-            });
         });
-
+        tvSignup.setOnClickListener(v -> {
+            startActivity(new Intent(login.this, dangky.class));
+        });
     }
 }
